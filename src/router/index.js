71e6149098f9b547a20b8bcb85180321546ef2af
1016297824 <1016297824@qq.com>
2020-01-30 17:20:29 +0800

    2020.01.30

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,32 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: Home
+    path: "",
+    name: "",
+    redirect: "/homepage"
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/homepage",
+    name: "homepage",
+    meta: {
+      title: "首页"
+    },
+    component: () => import("@/views/homepage/Login.vue")
   }
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: "history"
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.matched[0].meta.title;
+  next();
 });
 
 export default router;
