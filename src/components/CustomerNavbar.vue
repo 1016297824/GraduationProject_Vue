eb9139daa477393477bb0848272229acbdb4732d
1016297824 <1016297824@qq.com>
2020-02-07 20:32:17 +0800

    2020.02.07

--- src/components/CustomerNavbar.vue
+++ src/components/CustomerNavbar.vue
@@ -1,36 +1,35 @@
 // 顾客导航
 <template>
   <div id="navbar fixed-top">
-    <nav class="navbar navbar-dark bg-primary ">
+    <nav class="navbar bg-primary navbar-dark" style="height:53px">
       <span class="navbar-brand mb-0 h1">
         <img src="@/assets/imgs/Windows12.jpg" width="30" height="30" alt="" />
         西塘草舍农场
       </span>
       <ul class="nav justify-content-end">
         <li class="nav-item">
-          <a class="nav-link" href="reserve" style="color:#FFFFFF">
+          <a class="nav-link text-light" href="reserve">
             预定
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="order" style="color:#FFFFFF">
+          <a class="nav-link text-light" href="order">
             点餐
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="purchase" style="color:#FFFFFF">
+          <a class="nav-link text-light" href="purchase">
             采购
           </a>
         </li>
         <li class="nav-item dropdown">
           <a
-            class="nav-link dropdown-toggle"
+            class="nav-link dropdown-toggle text-white"
             data-toggle="dropdown"
             href="#"
             role="button"
             aria-haspopup="true"
             aria-expanded="false"
-            style="color:#FFFFFF"
           >
             <span> {{ cname }} </span>
           </a>
--- src/components/LoginNavbar.vue
+++ src/components/LoginNavbar.vue
@@ -1,7 +1,7 @@
 // 登录导航栏
 <template>
   <div id="navbar fixed-top">
-    <nav class="navbar navbar-dark bg-primary">
+    <nav class="navbar bg-primary navbar-dark" style="height:53px">
       <span class="navbar-brand mb-0 h1">
         <img src="@/assets/imgs/Windows12.jpg" width="30" height="30" alt="" />
         西塘草舍农场
